d664d08af88703b33a88ff405ac239efdd91026c
bbonnet <none>
2014-07-04 14:15:36 +0200

    Add basic velocity template and tests

---
+++ src/main/java/org/bbo/armybuilder/dataloader/DataLoader.java
@@ -0,0 +1,45 @@
+package org.bbo.armybuilder.dataloader;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.StringWriter;
+
+import org.apache.velocity.Template;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.VelocityEngine;
+import org.bbo.armybuilder.model.Army;
+import org.yaml.snakeyaml.TypeDescription;
+import org.yaml.snakeyaml.Yaml;
+import org.yaml.snakeyaml.constructor.Constructor;
+
+public class DataLoader {
+	public static Army load(String yamlPath) throws FileNotFoundException {
+		final Constructor constructor = new Constructor(Army.class);
+
+		final TypeDescription armyDescription = new TypeDescription(Army.class);
+		constructor.addTypeDescription(armyDescription);
+
+		final Yaml yaml = new Yaml(constructor);
+
+		final FileInputStream fileInputStream = new FileInputStream(new File(yamlPath));
+		final Army army = (Army) yaml.load(fileInputStream);
+
+		return army;
+	}
+
+	public static String fillTemplateWithArmyData(String templatePath, Army army) {
+		VelocityEngine velocityEngine = new VelocityEngine();
+		velocityEngine.init();
+
+		Template template = velocityEngine.getTemplate(templatePath);
+
+		VelocityContext velocityContext = new VelocityContext();
+		velocityContext.put("units", army.getUnits());
+
+		StringWriter stringWriter = new StringWriter();
+		template.merge(velocityContext, stringWriter);
+
+		return stringWriter.toString();
+	}
+}
--- src/test/java/org/bbo/armybuilder/dataloader/DataLoaderTest.java
+++ src/test/java/org/bbo/armybuilder/dataloader/DataLoaderTest.java
@@ -2,33 +2,31 @@ package org.bbo.armybuilder.dataloader;
 
 import static org.fest.assertions.Assertions.assertThat;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
 import org.bbo.armybuilder.model.Army;
+import org.bbo.armybuilder.model.Unit;
 import org.junit.Test;
-import org.yaml.snakeyaml.TypeDescription;
-import org.yaml.snakeyaml.Yaml;
-import org.yaml.snakeyaml.constructor.Constructor;
 
 public class DataLoaderTest {
 
 	@Test
-	public void test() throws FileNotFoundException {
-
-		final Constructor constructor = new Constructor(Army.class);
-
-		final TypeDescription armyDescription = new TypeDescription(Army.class);
-		constructor.addTypeDescription(armyDescription);
-
-		final Yaml yaml = new Yaml(constructor);
-
-		final FileInputStream fileInputStream = new FileInputStream(new File("src/main/resources/test.yaml"));
-		final Army army = (Army) yaml.load(fileInputStream);
+	public void should_return_army_with_units_when_loading_army() throws FileNotFoundException {
+		final Army army = DataLoader.load("src/main/resources/test.yaml");
 
 		System.out.println(army);
 		assertThat(army.getUnits().size() > 0);
 	}
 
+	@Test
+	public void should_return_string_with_unit_names_when_filling_template_with_army_data() throws FileNotFoundException {
+		Army army = DataLoader.load("src/main/resources/test.yaml");
+		String armyString = DataLoader.fillTemplateWithArmyData("src/main/resources/armyBook.html", army);
+
+		System.out.println(armyString);
+
+		for (Unit unit : army.getUnits()) {
+			assertThat(armyString).contains(unit.getName());
+		}
+	}
 }
