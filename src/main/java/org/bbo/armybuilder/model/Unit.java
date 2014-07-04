e6033dee54e4c1e372397a6490a719a05319b998
bbonnet <none>
2014-07-04 17:59:11 +0200

    Add rule in units

--- src/main/java/org/bbo/armybuilder/model/Unit.java
+++ src/main/java/org/bbo/armybuilder/model/Unit.java
@@ -1,10 +1,12 @@
 package org.bbo.armybuilder.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Unit {
 	private String name;
-	private List<Profile> profiles;
+	private List<Profile> profiles = new ArrayList<Profile>();
+	private List<Rule> rules = new ArrayList<Rule>();
 
 	public String getName() {
 		return name;
@@ -18,11 +20,22 @@ public class Unit {
 	public void setProfiles(final List<Profile> profiles) {
 		this.profiles = profiles;
 	}
+	public List<Rule> getRules() {
+		return rules;
+	}
+	public void setRules(List<Rule> rules) {
+		this.rules = rules;
+	}
 	@Override
 	public String toString() {
 		StringBuilder unitRepresentation = new StringBuilder(name + "\n");
+
 		for (Profile profile : profiles) {
-			unitRepresentation.append("    " + profile.getName() + profile.toString() + "\n");
+			unitRepresentation.append(" " + profile.getName() + profile.toString() + "\n");
+		}
+
+		for (Rule rule : rules) {
+			unitRepresentation.append(" " + rule.getName() + "\n");
 		}
 		return unitRepresentation.toString();
 	}
