cdf0d7f2e463bf40fa3ee37952bbcd8da6367eec
bbonnet <none>
2014-07-04 17:09:46 +0200

    Add name and rules properties to Army

--- src/main/java/org/bbo/armybuilder/model/Army.java
+++ src/main/java/org/bbo/armybuilder/model/Army.java
@@ -4,7 +4,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Army {
+	private String name;
 	private List<Unit> units = new ArrayList<Unit>();
+	private List<Rule> rules = new ArrayList<Rule>();
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 
 	public List<Unit> getUnits() {
 		return units;
@@ -14,12 +24,26 @@ public class Army {
 		this.units = units;
 	}
 
+	public List<Rule> getRules() {
+		return rules;
+	}
+
+	public void setRules(List<Rule> rules) {
+		this.rules = rules;
+	}
+
 	@Override
 	public String toString() {
 		StringBuilder stringRepresentation = new StringBuilder("Army:\n");
+
 		for(Unit unit : units){
 			stringRepresentation.append("  " + unit.toString() + "\n");
 		}
+
+		for (Rule rule : rules){
+			stringRepresentation.append(" " + rule.toString() + "\n");
+		}
+
 		return stringRepresentation.toString();
 	}
 }
