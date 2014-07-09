9eacf352d65c1018f120c98c06668b1908c43469
bbonnet <none>
2014-07-09 18:41:11 +0200

    Add exception type for data error

---
+++ src/main/java/org/bbo/armybuilder/dataloader/error/InconsistentDataException.java
@@ -0,0 +1,9 @@
+package org.bbo.armybuilder.dataloader.error;
+
+public class InconsistentDataException extends RuntimeException {
+
+	private static final long serialVersionUID = -4806659023591883985L;
+
+
+
+}
