ce1aaa25625ad22bf7bc70e9c0dac10ab03b4721
bbonnet <none>
2014-07-13 01:21:29 +0200

    Refactored test

--- src/test/java/org/bbo/armybuilder/dataloader/DataLoaderTest.java
+++ src/test/java/org/bbo/armybuilder/dataloader/DataLoaderTest.java
@@ -11,25 +11,32 @@ import org.junit.Test;
 
 public class DataLoaderTest {
 
+	private static final String SAMPLE_YAML = "src/main/resources/test.yaml";
+
 	@Test
 	public void should_return_army_with_units_when_loading_army() throws FileNotFoundException {
-		final Army army = DataLoader.load("src/main/resources/test.yaml");
+		// WHEN
+		final Army army = DataLoader.load(SAMPLE_YAML);
 
-		System.out.println(army);
+		// THEN
 		assertThat(army.getAllUnits().size() > 0);
 	}
 
 	@Test
 	public void should_return_string_with_unit_names_when_filling_template_with_army_data() throws FileNotFoundException {
-		Army army = DataLoader.load("src/main/resources/test.yaml");
+		// GIVEN
+		Army army = DataLoader.load(SAMPLE_YAML);
+		
+		// WHEN
 		String armyString = DataLoader.fillTemplateWithArmyData("src/main/resources/armyBook.html", army);
 
-		System.out.println(army.toString());
 
+		// THEN
 		for (Unit unit : army.getAllUnits()) {
 			assertThat(armyString).contains(unit.getName());
 		}
 
+		System.out.println(army.toString());
 		PrintWriter out = new PrintWriter("army-test.html");
 		out.println(armyString);
 		out.close();
