3a3622fd1f7885661ba42d00c399426676d90e2b
bbonnet <none>
2014-07-13 18:26:21 +0200

    [model] Fix range and strengh type in Weapon

--- src/main/java/org/bbo/armybuilder/model/Weapon.java
+++ src/main/java/org/bbo/armybuilder/model/Weapon.java
@@ -3,27 +3,34 @@ package org.bbo.armybuilder.model;
 import java.util.List;
 
 public class Weapon {
-	private int range;
-	private int strengh;
+	private String range;
+	private String strengh;
 	private List<String> rules;
-	public int getRange() {
+
+	public String getRange() {
 		return range;
 	}
-	public void setRange(int range) {
+
+	public void setRange(String range) {
 		this.range = range;
 	}
-	public int getStrengh() {
+
+	public String getStrengh() {
 		return strengh;
 	}
-	public void setStrengh(int strengh) {
+
+	public void setStrengh(String strengh) {
 		this.strengh = strengh;
 	}
+
 	public List<String> getRules() {
 		return rules;
 	}
+
 	public void setRules(List<String> rules) {
 		this.rules = rules;
 	}
+
 	@Override
 	public String toString() {
 		return "Weapon [range=" + range + ", strengh=" + strengh + ", rules="
